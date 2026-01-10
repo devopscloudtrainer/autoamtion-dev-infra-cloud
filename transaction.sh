cd72f5d2f125ecb41fd2ca653db71574f73a2e56
devopscloudtrainer <119645417+devopscloudtrainer@users.noreply.github.com>
2026-01-10 07:07:50 +0530

    Create transaction.sh

---
+++ transaction.sh
@@ -0,0 +1,17 @@
+Tag is an unique identification , label or bookmark for marking the commit as stable point of release.
+
+eg: v1, v1.0, v2.0, Z1.0, R1.0 etc
+    flipkart-prod-fix, release-stable-version-Q1 etc
+
+eg: commit-1
+    commit-2
+    commit-3
+    .
+    .
+    .
+    commit-50 - Everything is working fine - tested properly -> tag - v1.0, bug-fix-release-flipkart, prod-fix-2.0 etc
+    .
+    .
+    .
+    .
+    commit-100
